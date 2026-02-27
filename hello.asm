6e9a551e81b7b31d7a6da135e1df067c49d288d4
udaykiriti <udaykiriti624@gmail.com>
2026-02-27 14:16:15 +0530

    Hello World! just started

--- hello.asm
+++ hello.asm
@@ -1,22 +1,19 @@
-; SUPER SIMPLE HELLO WORLD
 
 section .data
     ; Just write the whole string at once
-    msg db "Hello World!", 10  ; 10 = newline
+    msg db "Hello World!", 0xa  ; 10 = newline[we can use that tooo]
     len equ $ - msg
 
 section .text
     global _start
 
 _start:
-    ; Ask OS to print our message
-    mov rax, 1      ; System call for write
-    mov rdi, 1      ; File handle 1 = screen
-    mov rsi, msg    ; Address of message
-    mov rdx, len    ; Length of message
-    syscall         ; Call the OS
-    
-    ; Ask OS to exit
-    mov rax, 60     ; System call for exit
-    xor rdi, rdi    ; Exit code 0 = success
-    syscall         ; Call the OS
+    mov edx , len  ;Length of msg
+    mov ecx , msg  ;msg to write
+    mov ebx , 1    ;File descrip [stdout]
+    mov eax , 4    ;sys call num [sys_write]
+    int 0x80       ;calls the kernel
+
+
+    mov eax , 1    ;syscall num [sys_exit]
+    int 0x80       ;call kernel
