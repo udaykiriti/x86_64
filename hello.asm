767bd1e166ea954ffa7c48ba20f8030b64bc530b
udaykiriti <udaykiriti624@gmail.com>
2026-02-27 14:34:50 +0530

    Use 64-bit syscalls in hello.asm for correct exit status

--- hello.asm
+++ hello.asm
@@ -8,13 +8,13 @@ section .text
     global _start
 
 _start:
-    mov edx , len  ;Length of msg
-    mov ecx , msg  ;msg to write
-    mov ebx , 1    ;File descrip [stdout]
-    mov eax , 4    ;sys call num [sys_write]
-    int 0x80       ;calls the kernel
+    mov rdx , len  ;Length of msg
+    mov rsi , msg  ;msg to write
+    mov rdi , 1    ;File descrip [stdout]
+    mov rax , 1    ;sys call num [sys_write]
+    syscall       ;calls the kernel
 
 
-    mov eax , 1    ;syscall num [sys_exit]
+    mov rax , 60    ;syscall num [sys_exit]
     mov rdi , 0
-    int 0x80       ;call kernel
+    syscall       ;call kernel
