77963fbef6efbdef45ec22f1de7a01a5d20499e1
udaykiriti <udaykiriti624@gmail.com>
2026-03-01 07:28:55 +0530

    Implemented add() and multiply() in src/math_utils.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
+/*
+ * Main entry point for the application.
+ * Calls into assembly routines defined in src/hello.asm.
+ */
+
+#include <stdio.h>
+#include "math_utils.h"
+
 extern void _hello(void);
 
 int main(void) {
     _hello();
+
+    int a = 10;
+    int b = 5;
+    
+    int sum = add(a, b);
+    int prod = multiply(a, b);
+
+    printf("Math in C: %d + %d = %d\n", a, b, sum);
+    printf("Math in C: %d * %d = %d\n", a, b, prod);
+
     return 0;
 }
