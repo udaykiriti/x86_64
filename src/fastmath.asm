149946f92a2abc67afc76d25625eacd4480608da
udaykiriti <udaykiriti624@gmail.com>
2026-03-02 18:45:59 +0530

    optimize asm_factorial with iterative loop

--- src/fastmath.asm
+++ src/fastmath.asm
@@ -25,29 +25,21 @@ asm_sub:
     sub eax, esi
     ret
 
-; Factorial (recursive): asm_factorial(int n)
+; Factorial (iterative): asm_factorial(int n)
 ; Parameter: edi = n
 ; Returns: eax = n!
 asm_factorial:
-    cmp edi, 1          ; if (n <= 1)
-    jle .return_one     ; return 1
-
-    ; n > 1: recursive case
-    push rbp
-    mov rbp, rsp
-    push rbx            ; save n for later use
-    
-    mov rbx, rdi        ; rbx = n
-    dec rdi              ; rdi = n - 1
-    call asm_factorial  ; eax = factorial(n-1)
-    imul eax, ebx       ; eax = eax * n
-    
-    pop rbx
-    pop rbp
-    ret
-
-.return_one:
     mov eax, 1
+    cmp edi, 1          ; if (n <= 1), return 1
+    jle .done
+
+.loop:
+    imul eax, edi       ; eax *= n
+    dec edi             ; n--
+    cmp edi, 1
+    jg .loop
+
+.done:
     ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
