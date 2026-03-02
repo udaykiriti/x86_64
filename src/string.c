1ff402f8659544e65abed06cec7710cfb3e2cf35
udaykiriti <udaykiriti624@gmail.com>
2026-03-02 18:40:09 +0530

    improve string helpers with defensive guards

--- src/string.c
+++ src/string.c
@@ -1,6 +1,10 @@
 #include "string.h"
 
 int str_len(const char *str) {
+    if (str == 0) {
+        return 0;
+    }
+
     int length = 0;
     while (str[length] != '\0') {
         length++;
@@ -9,15 +13,19 @@ int str_len(const char *str) {
 }
 
 void str_rev(char *str) {
+    if (str == 0 || str[0] == '\0' || str[1] == '\0') {
+        return;
+    }
+
     int length = str_len(str);
     int start = 0;
     int end = length - 1;
 
     while (start < end) {
-        char temp = str[start];
+        char temp_char = str[start];
         str[start] = str[end];
-        str[end] = temp;
-        
+        str[end] = temp_char;
+
         start++;
         end--;
     }
