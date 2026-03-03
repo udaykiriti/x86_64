848c09d221ad21b95f3da7f934b655869673cdc2
udaykiriti <udaykiriti624@gmail.com>
2026-03-03 09:22:19 +0530

    make C factorial iterative and optimize in-place string reversal

--- src/string.c
+++ src/string.c
@@ -13,19 +13,22 @@ int str_len(const char *str) {
 }
 
 void str_rev(char *str) {
-    if (str == 0 || str[0] == '\0' || str[1] == '\0') {
+    if (str == 0 || str[0] == '\0') {
         return;
     }
 
-    int length = str_len(str);
-    int start = 0;
-    int end = length - 1;
+    char *start = str;
+    char *end = str;
+
+    while (*end != '\0') {
+        end++;
+    }
+    end--;
 
     while (start < end) {
-        char temp_char = str[start];
-        str[start] = str[end];
-        str[end] = temp_char;
-
+        char temp_char = *start;
+        *start = *end;
+        *end = temp_char;
         start++;
         end--;
     }
