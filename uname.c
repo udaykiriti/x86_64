0a8420dfa890110511c03d4702f2f47690d62eca
udaykiriti <udaykiriti624@gmail.com>
2026-04-14 02:23:25 +0530

    some ram details
    Signed-off-by: udaykiriti <udaykiriti624@gmail.com>

--- uname.c
+++ uname.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <sys/utsname.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
 
 int main() {
-    struct utsname sysinfo;
+    struct utsname u;
+    struct sysinfo s;
 
+    uname(&u);
+    sysinfo(&s);
+    /*
     if (uname(&sysinfo) == -1) {
         perror("uname");
         return 1;
-    }
+    } */
 
-    printf("System Name : %s\n", sysinfo.sysname);
-    printf("Node Name   : %s\n", sysinfo.nodename);
-    printf("Release     : %s\n", sysinfo.release);
-    printf("Version     : %s\n", sysinfo.version);
-    printf("Machine     : %s\n", sysinfo.machine);
+    printf("System Name : %s\n", u.sysname);
+    printf("Node Name   : %s\n", u.nodename);
+    printf("Release     : %s\n", u.release);
+    printf("Version     : %s\n", u.version);
+    printf("Machine     : %s\n", u.machine);
+
+    printf("Tot ram : %lu MB\n",s.totalram/(1024/1024));
+    
+    printf("Avbl ram : %lu MB\n",s.freeram/(1024/1024));
 
     return 0;
 }
