97cb187cc3d4b9ac2d281e6a128e221a5016ca3e
udaykiriti <udaykiriti624@gmail.com>
2026-04-14 02:11:42 +0530

    header files and func
      - process [fork]
      - syscalls
      - mmap
      - pipes
    
    Signed-off-by: udaykiriti <udaykiriti624@gmail.com>

---
+++ mmap.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/mman.h>   // mmap
+#include <unistd.h>     // getpagesize
+#include <string.h>
+
+int main() {
+    size_t size = getpagesize();  // usually 4096 bytes
+
+    // Req mem from kernel
+    void *mem = mmap(
+        /*we hope and pray for that */
+        NULL,                   // let kernel choose address
+        size,                   // sz
+        PROT_READ | PROT_WRITE, // permissions
+        MAP_PRIVATE | MAP_ANONYMOUS,
+        -1,                     // no file
+        0
+    );
+
+    if (mem == MAP_FAILED) {
+        perror("mmap failed");
+        return 1;
+    }
+
+    printf("Allocated memory at: %p\n", mem);
+
+    // wr into mapped memory
+    strcpy((char*)mem, "Hello from mmap!");
+
+    printf("Data in memory: %s\n", (char*)mem);
+
+    // free(memory)
+    munmap(mem, size);
+
+    return 0;
+}
---
+++ shell.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>     // fork, execvp
+#include <sys/wait.h>   // waitpid
+#include <string.h>
+
+#define MAX 1024
+
+int main() {
+    char input[MAX];
+
+    while (1) {
+        printf("mysh> ");
+        fflush(stdout);
+
+        if (!fgets(input, MAX, stdin)) break;
+
+        input[strcspn(input, "\n")] = 0;
+
+        // exit
+        if (strcmp(input, "exit") == 0) break;
+
+        // Tokenize input into args
+        char *args[64];
+        int i = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        // Fork process
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            // Child process
+            execvp(args[0], args);
+            perror("exec failed");
+            exit(1);
+        } 
+        else if (pid > 0) {
+            // Parent process
+            waitpid(pid, NULL, 0);
+        } 
+        else {
+            perror("fork failed");
+        }
+    }
+
+    return 0;
+}
